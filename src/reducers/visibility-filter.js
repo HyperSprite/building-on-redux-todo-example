b15753bb61187679b358f89fdc515c29dc62328d
Christopher Dunavan <github@hypersprite.com>
2016-11-14 17:58:00 -0800

    fixed SET_VISIBILITY_FILTER

--- src/reducers/visibility-filter.js
+++ src/reducers/visibility-filter.js
@@ -1,9 +1,9 @@
 // @flow
-import { SET_VISIBILITY_FILTER } from '../actions';
+import { TYPES } from '../actions';
 
 function visibilityFilter(state = 'SHOW_ALL', action) {
   switch (action.type) {
-    case SET_VISIBILITY_FILTER:
+    case TYPES.SET_VISIBILITY_FILTER:
       return action.filter;
     default:
       return state;
