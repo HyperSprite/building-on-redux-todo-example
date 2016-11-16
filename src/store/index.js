26c7c50bb2f18fb34ebc40048a1c5ec765e6a400
Christopher Dunavan <github@hypersprite.com>
2016-11-15 17:42:10 -0800

    Added redux-dev-tools and moved logger to 'if' dev
    These are on an if "dev" condition as well as the redux logger
    So production should run clean without devtools or logging

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import logger from 'redux-logger';
 
 import reducers from '../reducers';
 
-const middleware = applyMiddleware(
+const prodMiddleware = [
+  promise(),
+  thunk,
+];
+
+const devMiddleware = [
   promise(),
   thunk,
   logger(),
-);
+];
 
-export default createStore(reducers, {}, middleware);
+let middlewareOptions;
+
+if (process.env.NODE_ENV === 'production') {
+  middlewareOptions = applyMiddleware(...prodMiddleware);
+} else {
+  middlewareOptions = composeWithDevTools(
+    applyMiddleware(...devMiddleware),
+    // other store enhancers if any
+  );
+}
+
+const store = createStore(reducers, middlewareOptions);
+
+export default store;
